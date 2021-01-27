b61d22a897011b6c73b0ec7814b0b3c922f15137
Jonathan Fabris <jonfaal@gmail.com>
2021-01-27 13:33:19 -0300

    Implementação da função da agulha e correção do play multiplo.

--- javascript.js
+++ javascript.js
@@ -5,52 +5,74 @@ var intervaloVinil;
 var intervaloAgulha;
 let rot = 0;
 let ang = 0;
+var state = "stop";
+
 
 
 function play(){
-    music.play()
-
     if(ang == 0){
-    ang = 12;
+        ang = 12;
     }
     
     agulha.style = "transform: rotate("+ang+"deg)"; 
+    let passoAgulha = music.duration / 44;
+
+    function tocar(){
+        music.play();
+        intervaloAgulha = setInterval(function(){
+            ang += 0.5;
+            agulha.style = "transform: rotate("+ang+"deg)"; 
     
-    intervaloAgulha = setInterval(function (){
-        ang ++
-        agulha.style = "transform: rotate("+ang+"deg)"; 
-    }, 2000);
+            if(ang == 34){
+    
+                ang = 40; 
+                setTimeout(stop, 3000)
+            }
+    
+            console.log(ang)
+        }, passoAgulha*1000);
+    }
 
-    intervaloVinil = setInterval(function (){
-        rot ++
-        vinil.style = "transform: rotate("+rot+"deg)"; 
+    if(state !== "play"){
+        
+        if(state == "stop"){
+            setTimeout(tocar, 1000);
+        }else{
+            tocar();
+        }
 
-        console.log(music.duration)
-    }, 1);
+        intervaloVinil = setInterval(function (){
+            rot ++
+            vinil.style = "transform: rotate("+rot+"deg)"; 
+        }, 1);
+
+        state = "play";
+    }
+
+
+
 }
 
 
-
 function pause(){
+    state = "pause";
     music.pause();
     clearInterval(intervaloVinil);
-    intervaloVinil = null
     clearInterval(intervaloAgulha);
     rot += 10;
     vinil.style = "transition: 1s; transform: rotate("+rot+"deg)"; 
     
-    console.log(rot)
 }
 
 function stop(){
+    state = "stop";
     music.pause()
     music.currentTime = 0;
 
     agulha.style = "transform: rotate(0deg)"; 
 
-
     clearInterval(intervaloVinil);
     clearInterval(intervaloAgulha);
-    vinil.style = "transition: 0s; transform: rotate(0deg)"; 
-    console.log('stop')
+    ang = 0
+    vinil.style = "transition: 0s; transform: rotate("+ang+"deg)"; 
 }

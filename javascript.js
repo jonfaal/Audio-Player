c6fa252b3d13da4f5fc1c58d0baacae55cb19c16
Jonathan Fabris <jonfaal@gmail.com>
2021-01-29 16:52:13 -0300

    Nova musica e ajuste na agulha

--- javascript.js
+++ javascript.js
@@ -12,84 +12,77 @@ let rot = 0;
 let ang = 0;
 var state = "stop";
 
-
-
-function play(){
-    if(ang == 0){
+function play() {
+    if (ang == 0) {
         ang = 12;
     }
-    
-    agulha.style = "transform: rotate("+ang+"deg)"; 
+
+    agulha.style = "transform: rotate(" + ang + "deg)";
     let passoAgulha = music.duration / 44;
 
-    function tocar(){
+    function tocar() {
         music.play();
-        intervaloAgulha = setInterval(function(){
+        intervaloAgulha = setInterval(function () {
             ang += 0.5;
-            agulha.style = "transform: rotate("+ang+"deg)"; 
-    
-            if(ang == 34){
-    
-                ang = 40; 
-                agulha.style = "transition: 1s; transform: rotate("+ang+"deg)"; 
-                
+            agulha.style = "transform: rotate(" + ang + "deg)";
+
+            if (ang == 34) {
+
+                ang = 40;
+                agulha.style = "transition: 1s; transform: rotate(" + ang + "deg)";
+
                 setTimeout(stop, 2000)
             }
-    
-        }, passoAgulha*1000);
+
+        }, passoAgulha * 1000);
     }
 
-    if(state !== "play"){
-        
-        if(state == "stop"){
+    if (state !== "play") {
+
+        if (state == "stop") {
             setTimeout(tocar, 1000);
-        }else{
+        } else {
             tocar();
         }
 
-        intervaloVinil = setInterval(function (){
-            rot ++
-            vinil.style = "transform: rotate("+rot+"deg)"; 
+        intervaloVinil = setInterval(function () {
+            rot++
+            vinil.style = "transform: rotate(" + rot + "deg)";
         }, 1);
 
         state = "play";
-        btnPlay.className ="botao inner-shadow";
-        btnPause.className ="botao outer-shadow";
-        btnStop.className ="botao outer-shadow";
+        btnPlay.className = "botao inner-shadow";
+        btnPause.className = "botao outer-shadow";
+        btnStop.className = "botao outer-shadow";
     }
-
-
-
 }
 
-
-function pause(){
-    if(state == 'play'){
+function pause() {
+    if (state == 'play') {
         state = "pause";
-        btnPlay.className ="botao outer-shadow";
-        btnPause.className ="botao inner-shadow";
-        btnPlay.className ="botao outer-shadow";
+        btnPlay.className = "botao outer-shadow";
+        btnPause.className = "botao inner-shadow";
+        btnPlay.className = "botao outer-shadow";
         music.pause();
         clearInterval(intervaloVinil);
         clearInterval(intervaloAgulha);
         rot += 10;
-        vinil.style = "transition: 1s; transform: rotate("+rot+"deg)"; 
+        vinil.style = "transition: 1s; transform: rotate(" + rot + "deg)";
     }
-    
 }
 
-function stop(){
+function stop() {
     state = "stop";
-    btnPlay.className ="botao outer-shadow";
-    btnPause.className ="botao outer-shadow";
-    btnStop.className ="botao inner-shadow";
+    btnPlay.className = "botao outer-shadow";
+    btnPause.className = "botao outer-shadow";
+    btnStop.className = "botao inner-shadow";
     music.pause()
     music.currentTime = 0;
 
-    agulha.style = "transform: rotate(0deg)"; 
+    agulha.style = "transform: rotate(0deg)";
 
     clearInterval(intervaloVinil);
     clearInterval(intervaloAgulha);
     ang = 0
-    vinil.style = "transition: 0s; transform: rotate("+ang+"deg)"; 
+    vinil.style = "transition: 0s; transform: rotate(" + ang + "deg)";
 }
